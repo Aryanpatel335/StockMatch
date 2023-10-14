8b1a97b7927c1046277591872565f991a1c57ef0
Aryanpatel335 <aryanp862002@gmail.com>
2023-10-13 22:26:04 -0400

    spring boot initilze backend (postgres)

---
+++ backendStockMatch/src/main/java/com/backend/backendStockMatch/BackendStockMatchApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backendStockMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStockMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStockMatchApplication.class, args);
+	}
+
+}
