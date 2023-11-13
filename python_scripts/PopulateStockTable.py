aaa9c645aa1d641a88893ec20635ffa71440a573
aryan <aryanp862002@gmail.com>
2023-11-13 12:06:23 -0500

    python script to populate stock table

---
+++ python_scripts/PopulateStockTable.py
@@ -0,0 +1,15 @@
+import requests
+import time
+
+
+with open('resources/sp500_symbols.txt', 'r') as file:
+    symbols = file.read().splitlines()
+
+# Base URL
+base_url = 'http://localhost:3000/quote'
+
+for symbol in symbols:
+    params = {'symbol': symbol}
+    response = requests.get(base_url, params=params)
+    print(response)
+   
