b95596622634359a5b852e37214dc0ee4a6c2714
aryan <aryanp862002@gmail.com>
2023-11-14 11:44:09 -0500

    added initial go handler for stockCandle

--- StockMatchBackend/src/main/java/Backend/StockMatchBackend/controller/StockCandleController.java
+++ StockMatchBackend/src/main/java/Backend/StockMatchBackend/controller/StockCandleController.java
@@ -3,6 +3,7 @@ package Backend.StockMatchBackend.controller;
 import Backend.StockMatchBackend.model.StockCandle;
 import Backend.StockMatchBackend.repository.StockCandleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
@@ -26,4 +27,14 @@ public class StockCandleController {
     }
 
     // Additional CRUD operations (update, delete) can be added here
+
+    //sample (using for testing the string received for json data)
+    @PostMapping("/receiveStockQuote")
+    public ResponseEntity<String> receiveStockQuote(@RequestBody String stockCandle) {
+        // Print the received JSON
+        System.out.println("Received stock quote: " + stockCandle);
+
+        // Return a response
+        return ResponseEntity.ok("Received stock quote: " + stockCandle);
+    }
 }
--- go_microservice/main.go
+++ go_microservice/main.go
@@ -9,5 +9,6 @@ import (
 
 
 func main() {
-	handlers.StockQuoteHandler()
+	// handlers.StockQuoteHandler()
+	handlers.StockCandlesHandler()
 }
