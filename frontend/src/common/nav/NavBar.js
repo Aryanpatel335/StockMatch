e8280679035b86a042e64da92cdd1ce7e785835a
martinivnv <martinivnv2002@gmail.com>
2023-12-15 20:10:21 -0500

    Visual tweaks to navbar

--- frontend/src/common/nav/NavBar.js
+++ frontend/src/common/nav/NavBar.js
@@ -43,8 +43,8 @@ const NavBar = (props) => {
 
 	return (
 		<>
-			<Box px={4} boxShadow="md">
-				<Flex h={12} alignItems={"center"} justifyContent={"space-between"}>
+			<Box h={14} px={4} boxShadow="md">
+				<Flex h={14} alignItems={"center"} justifyContent={"space-between"}>
 					{!isPreferencesPage ? (
 						<HStack
 							spacing={8}
@@ -71,6 +71,7 @@ const NavBar = (props) => {
 								display={{ base: "none", md: "flex" }}
 								justifyContent={"flex-end"}
 								overflowX={"hidden"}
+								overflowY={"hidden"}
 							>
 								{Links.map((l) => (
 									<Link to={l.link}>
