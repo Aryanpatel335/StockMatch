96e1f82851263f0614443b17267533eadc03549a
aryan <aryanp862002@gmail.com>
2023-12-21 17:08:21 -0500

    database config file changes env

--- StockMatchBackend/src/main/java/Backend/StockMatchBackend/StockMatchBackendApplication.java
+++ StockMatchBackend/src/main/java/Backend/StockMatchBackend/StockMatchBackendApplication.java
@@ -3,31 +3,9 @@ package Backend.StockMatchBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 @SpringBootApplication
 public class StockMatchBackendApplication {
-
-	static {
-		try {
-			// Replace with the path to your .env file
-			List<String> lines = Files.readAllLines(Paths.get(".env"));
-			for (String line : lines) {
-				String[] parts = line.split("=", 2);
-				if (parts.length == 2) {
-					String key = parts[0];
-					String value = parts[1];
-					System.setProperty(key, value);
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to load .env file", e);
-		}
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockMatchBackendApplication.class, args);
---
+++ StockMatchBackend/src/main/java/Backend/StockMatchBackend/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package Backend.StockMatchBackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    // Getters and possibly setters for the fields
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+}
