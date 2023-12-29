66733f9458920d90a653dd338d458ac35ea66dd5
martinivnv <martinivnv2002@gmail.com>
2023-12-29 14:11:23 -0500

    Added loading spinner to login button

--- frontend/src/auth/Login.js
+++ frontend/src/auth/Login.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useDispatch } from "react-redux";
 import { googleSignInUser } from "../store/authSlice";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalBody,
+	Spinner,
+	Text,
+	Flex,
+} from "@chakra-ui/react";
 
 const Login = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
+	const [isLoading, setIsLoading] = useState(false);
 
 	const googleSuccess = async (res) => {
+		setIsLoading(true);
+
 		const data = jwtDecode(res.credential);
 
 		try {
@@ -16,6 +29,7 @@ const Login = () => {
 			const email = data.email;
 			const name = data.name;
 			const obj = { sub, email, name };
+
 			await fetch(`${process.env.REACT_APP_BACKEND}/users/login`, {
 				method: "POST",
 				headers: {
@@ -36,23 +50,54 @@ const Login = () => {
 					} else {
 						console.log("Not signed in successfully");
 					}
+				})
+				.finally(() => {
+					setIsLoading(false);
 				});
 		} catch (error) {
 			console.log(error);
+			setIsLoading(false);
 		}
 	};
+
 	const googleFailure = (error) => {
 		console.log(error);
 		console.log("Google Sign Up unsuccessful");
+		setIsLoading(false);
 	};
 
 	return (
-		<GoogleLogin
-			onSuccess={googleSuccess}
-			onError={googleFailure}
-			theme="filled_black"
-			text="continue_with"
-		/>
+		<>
+			<GoogleLogin
+				onSuccess={googleSuccess}
+				onError={googleFailure}
+				theme="filled_black"
+				text="continue_with"
+			/>
+
+			{/* Modal with Spinner */}
+			<Modal isOpen={isLoading} closeOnOverlayClick={false} isCentered>
+				<ModalOverlay />
+				<ModalContent
+					px={"1em"}
+					py={"2em"}
+					textAlign={"center"}
+					w={"20em"}
+					maxW={"70vw"}
+				>
+					<Flex
+						flexDirection="column"
+						alignContent="center"
+						justifyContent="center"
+					>
+						<ModalBody>
+							<Spinner size="xl" color={"#9C7CF4"} />
+							<Text>Connecting to server...</Text>
+						</ModalBody>
+					</Flex>
+				</ModalContent>
+			</Modal>
+		</>
 	);
 };
 
