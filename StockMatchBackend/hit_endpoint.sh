7555bdf6634399f24f606e44be5905f5c0b1a6ae
Aryanpatel335 <aryanp862002@gmail.com>
2023-12-28 21:26:58 -0500

    hit endpoint docker change test

---
+++ StockMatchBackend/hit_endpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Endpoint URL
+URL="https://stockmatch-spring-api.onrender.com/"
+
+# Send a GET request to the URL
+curl -s $URL
+
+# You can redirect the output to a log file or handle it in any other way if needed
